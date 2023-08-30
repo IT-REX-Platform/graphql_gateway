b624ae21fa3e4d19363a24f4599eca55d0a1f68f
myluki2000 <lukas636@live.com>
2023-08-30 17:27:20 +0200

    course-level suggestions query

---
+++ additionalResolvers/course.ts
@@ -0,0 +1,50 @@
+import {Resolvers} from "../.mesh";
+
+const resolvers: Resolvers = {
+    Course: {
+        suggestions: {
+            // manually request some fields from the chapters that we need to decide if the chapter should
+            // be included in the suggestions
+            selectionSet: `
+            {
+              chapters {
+                elements {
+                  id
+                  startDate
+                  suggestedStartDate
+                }
+              }
+            }
+            `,
+            async resolve(root, _args, context, info) {
+                let currentTime = Date.now();
+
+                let chapters = root.chapters.elements.filter(chapter => {
+                    // if chapter hasn't started yet, don't suggest it
+                    if(Date.parse(chapter.startDate) > currentTime) {
+                        return false;
+                    }
+
+                    // if chapter doesn't have a suggested start date, suggest it
+                    if(chapter.suggestedStartDate === null) {
+                        return true;
+                    }
+
+                    // if chapter has a suggested start date and it's in the past, suggest it
+                    return Date.parse(chapter.suggestedStartDate) < currentTime;
+                });
+
+                return await context.ContentService.Query.suggestionsByChapterIds({
+                    root,
+                    args: {
+                        chapterIds: chapters.map(chapter => chapter.id),
+                    },
+                    context,
+                    info
+                })
+            }
+        }
+    }
+};
+
+export default resolvers;
